87cb9f90dc5b337b9820e7f35e4fe60736f2608f
Taras <tarasenya@gmail.com>
2023-11-03 13:36:10 +0100

    * Added final model.

---
+++ MidTermProject/src/flask_model_serving_app.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from flask import Flask, request, jsonify
+from utils import get_preprocessor, get_lgbm_booster
+
+app = Flask('prediction_app')
+
+MODEL = get_lgbm_booster('lightbm_booster')
+PREPROCESSOR = get_preprocessor()
+
+
+@app.route('/predict', methods=['POST'])
+def prediction_end_point():
+    preprocessed_request = PREPROCESSOR.transform(pd.DataFrame([request.get_json()]))
+    res = {'res': float(MODEL.predict(preprocessed_request)),
+           'status': 'OK'}
+    return jsonify(res)
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=3141, debug=True)
