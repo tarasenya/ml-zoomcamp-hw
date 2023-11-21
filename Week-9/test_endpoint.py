54e1e97b9513963503f1c2b5f6a1ac1dace5c48d
Taras <tarasenya@gmail.com>
2023-11-21 23:18:29 +0100

    Added screenshots for aws deployment

--- Week-9/test_endpoint.py
+++ Week-9/test_endpoint.py
@@ -2,7 +2,9 @@ import requests
 
 url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
 
+aws_url = 'https://i9ndp3pykk.execute-api.us-east-1.amazonaws.com/test/predict'
+
 data = {'url': 'https://habrastorage.org/webt/rt/d9/dh/rtd9dhsmhwrdezeldzoqgijdg8a.jpeg'}
 
-result = requests.post(url, json=data).json()
+result = requests.post(aws_url, json=data).json()
 print(result)
